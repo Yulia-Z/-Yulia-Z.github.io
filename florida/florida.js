5f648f4273720eb9b4cae7117e07b3b4fcdb4e44
= <=>
2020-03-03 18:47:59 -0800

    small changes

--- florida/florida.js
+++ florida/florida.js
@@ -1,10 +1,10 @@
 function myMove() {
     const boat = document.getElementById("animation");
-    let pos = -125;
+    let pos = 0;
     const move = setInterval(frame, 20);
 
     function frame() {
-        if (pos === 0) {
+        if (pos === 125) {
             clearInterval(move);
         } else {
             pos ++;
