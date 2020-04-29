45da85f3d886b4e5adc7541f110e83a436895661
= <=>
2020-04-28 20:45:09 -0400

    added slider

--- florida/florida.js
+++ florida/florida.js
@@ -12,4 +12,43 @@ function myMove() {
         }
     }
 }
-//make the boat
+
+// find element
+const prevBtn = document.getElementById("show-prev-btn");
+const nextBtn = document.getElementById("show-next-btn");
+const slideImage = document.getElementById("slide-img");
+
+// subscribe to events
+prevBtn.addEventListener("click", onPrevBtnClick);
+nextBtn.addEventListener("click", onNextBtnClick);
+
+// create image array
+const imageSrcs = [];
+imageSrcs.push("./images/jacksonville.jpg");
+imageSrcs.push("./images/miami.jpeg");
+imageSrcs.push("./images/tampa.jpg");
+imageSrcs.push("./images/orlando.jpg");
+imageSrcs.push("./images/st-pete.jpeg");
+
+let currentImageIndex = 0;
+slideImage.src = imageSrcs[currentImageIndex];
+prevBtn.disabled = true;
+
+// function definitions
+function onPrevBtnClick() {
+    currentImageIndex--;
+    slideImage.src = imageSrcs[currentImageIndex];
+    nextBtn.disabled = false;
+    if (currentImageIndex === 0) {
+        prevBtn.disabled = true;
+    } 
+}
+
+function onNextBtnClick() {
+    currentImageIndex++;
+    slideImage.src = imageSrcs[currentImageIndex];
+    prevBtn.disabled = false;
+    if (currentImageIndex === imageSrcs.length - 1) {
+        nextBtn.disabled = true;
+    } 
+}
